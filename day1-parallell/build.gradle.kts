c08367d417bfd767487ac13517f03a384b46a018
Marius Brauterfallet <marius.brauterfallet@tietoevry.com>
2023-12-06 18:50:30 +0100

    day1 parallell

---
+++ day1-parallell/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
--- day1-parallell/src/main/kotlin/Main.kt
+++ day1-parallell/src/main/kotlin/Main.kt
@@ -1,5 +1,24 @@
+import kotlinx.coroutines.*
 import java.io.File
 
 fun main() {
-    val onelinerResult = File("src/main/resources/input.txt").readLines().sumOf { "${it.first { char -> char.isDigit() }}${it.last { char -> char.isDigit() }}".toInt() }.also { println("Part 1 result: $it") }
+    val onelinerResult = File("src/main/resources/input.txt").readLines()
+        .sumOf { "${it.first { char -> char.isDigit() }}${it.last { char -> char.isDigit() }}".toInt() }
+        .also { println("Part 1 result: $it") }
+
+    val input = File("src/main/resources/input.txt").readLines()
+
+    val result = runBlocking(Dispatchers.Default) {
+        input
+            .map { line ->
+                async {
+                    val first = line.first { it.isDigit() }
+                    val last = line.last { it.isDigit() }
+
+                    "$first$last".toInt()
+                }
+            }.sumOf { it.await() }
+    }
+
+    println("Parallell result: $result")
 }
